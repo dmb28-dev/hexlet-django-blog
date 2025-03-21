e1ab8a623ca100d7f18bd6a61f5db5da7343650a
Dmitriy <arcon280506@mail.ru>
2025-03-21 23:12:01 +0300

    lesson 2

--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from hexlet_django_blog import views
+from django.urls import path, include
 
 urlpatterns = [
 		path('', views.index),
     path('admin/', admin.site.urls),
 		path('about/', views.about),
+		path('articles/', include('hexlet_django_blog.article.urls')),
 ]
